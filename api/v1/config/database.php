a22c45ff731b0dfabb3e64e71a4ba0971ae9417e
Tuan Dat <tuandatdo.03@gmail.com>
2025-10-28 22:19:44 +0700

    tmp

--- api/v1/config/database.php
+++ api/v1/config/database.php
@@ -1,12 +1,14 @@
 <?php
 class Database
 {
+    // TODO: Database connection info must be kept secret
     private $host = "localhost";
     private $db_name = "persol_db";
-    private $username = "root";
-    private $password = "";
+    private $username = "admin";
+    private $password = "admin";
     public $conn;
 
+    // TODO: Use rand to generate secret key
     public static $jwt_key = "YOUR_SUPER_SECRET_KEY_123!@#";
     public static $jwt_iss = "persol.com";
     public static $jwt_aud = "persol.com";
@@ -20,10 +22,13 @@ class Database
             $this->conn = new PDO(
                 "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
                 $this->username,
-                $this->password
+                $this->password,
             );
             $this->conn->exec("set names utf8");
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION,
+            );
         } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
